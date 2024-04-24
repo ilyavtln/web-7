aaec0b75bcac93f42343450fc01d0525f002ef10
ilyavtln <owtf086@gmail.com>
2024-04-24 23:23:36 +0700

    first settings

---
+++ errors/404.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ошибка</title>
+    <meta name="description" content="Страница не найдена">
+    <link href="../styles/bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="page-section">
+    <div class="container">
+        <h1>Это страница ошибок</h1>
+        <h5>
+            Ошибка, такой страницы нет(
+        </h5>
+        <a class="btn btn-danger" href="../index.php">
+            Вернуться на главную
+        </a>
+    </div>
+</section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-# Главная страница
-<p class="dsd">
+<!doctype html>
+<html lang="ru">
 
-</p>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Страница акций</title>
+    <meta name="description" content="Акции и призы ежедневно">
+    <link href="styles/bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+<section class="page-section">
+    <div class="container">
+        <h1>Это главная страница</h1>
+        <h5>Это описание главной страницы</h5>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate delectus dicta earum excepturi harum hic inventore itaque labore magni, minima neque nihil odio reiciendis, sequi voluptates voluptatibus. Doloremque, repellat.
+        </p>
+    </div>
+</section>
+</body>
+</html>
