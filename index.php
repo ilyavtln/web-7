90a236c5935e4dcacc4dc41443f256257b6d5b9f
ilyavtln <owtf086@gmail.com>
2024-04-28 22:22:47 +0700

    nav add

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!doctype html>
 <html lang="ru">
 
@@ -6,17 +8,136 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Страница акций</title>
     <meta name="description" content="Акции и призы ежедневно">
+    <script src="scripts/js/bootstrap.js"></script>
     <link href="styles/bootstrap.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </head>
 
 <body>
+
+<section class="d-none d-md-block sticky-top">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md nav-underline bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img src="shared/logos/main.svg" alt="Logo" width="auto" height="50" class="d-inline-block align-text-center">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
+                    <ul class="navbar-nav gap-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Главная</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/catalog.php">Каталог</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/promo.php">Акции</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Дополнительно
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="pages/promo.php">Ввести промокод</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav gap-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/favorites.php">
+                                <i class="bi bi-heart"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/cabinet.php">
+                                <i class="bi bi-person-circle"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pages/cart.php">
+                                <i class="bi bi-bag"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</section>
+
+<section class="d-md-none sticky-top">
+    <div class="container-fluid px-0">
+        <nav class="navbar bg-body-tertiary border-1 border-bottom border-black justify-content-between">
+            <a class="navbar-brand px-2" href="index.php">
+                Акции
+            </a>
+            <ul class="navbar-nav flex-row gap-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/favorites.php">
+                        <i class="bi bi-heart"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/cabinet.php">
+                        <i class="bi bi-person-circle"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pages/cart.php">
+                        <i class="bi bi-bag"></i>
+                    </a>
+                </li>
+            </ul>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Акции</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body py-0">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Главная</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="pages/catalog.php">Каталог</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="pages/promo.php">Акции</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Дополнительно
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="pages/promo.php">Ввести промокод</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</section>
+
 <section class="page-section">
     <div class="container">
         <div class="bg-light rounded-3 p-3">
             <h1>Это главная страница</h1>
             <h5>Это описание главной страницы</h5>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate delectus dicta earum excepturi harum hic inventore itaque labore magni, minima neque nihil odio reiciendis, sequi voluptates voluptatibus. Doloremque, repellat.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cupiditate delectus dicta earum
+                excepturi harum hic inventore itaque labore magni, minima neque nihil odio reiciendis, sequi voluptates
+                voluptatibus. Doloremque, repellat.
             </p>
         </div>
     </div>
